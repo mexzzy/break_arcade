fad935f87eb96e27b2b8f3a3fe404132e143cb28
mexzzy <samuelmeshach055@gmail.com>
2023-12-25 00:21:23 +0100

    login screen

---
+++ lib/auth/login.dart
@@ -0,0 +1,119 @@
+import 'package:break_arcade/constants/app_colors.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: AppColors.primaryBg,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/breakArcade_logo.png',
+                height: 50,
+                width: 50,
+                fit: BoxFit.cover,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 16.0),
+                child: Text(
+                  'Break Arcade',
+                  style: TextStyle(
+                    fontSize: 35,
+                    fontWeight: FontWeight.bold,
+                    color: AppColors.white,
+                  ),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 5.0),
+                child: Text(
+                  'Sign in to continue',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: AppColors.white,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Container(
+                width: 0.8 * MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.only(
+                  top: 5,
+                  right: 10,
+                  left: 10,
+                  bottom: 5,
+                ),
+                decoration: const BoxDecoration(
+                  color: AppColors.inputBg,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      10,
+                    ),
+                  ),
+                ),
+                child: TextFormField(
+                  style: const TextStyle(color: AppColors.white),
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your email',
+                    hintStyle: TextStyle(
+                      color: AppColors.grayText,
+                    ),
+                    border: InputBorder.none,
+                  ),
+                  // validator: (String? value) {
+                  //   if (value == null || value.isEmpty) {
+                  //     return 'Please enter some text';
+                  //   }
+                  //   return null;
+                  // },
+                ),
+              ),
+              const SizedBox(height: 20),
+              Container(
+                width: 0.8 * MediaQuery.of(context).size.width,
+                padding: const EdgeInsets.only(
+                  top: 5,
+                  right: 10,
+                  left: 10,
+                  bottom: 5,
+                ),
+                decoration: const BoxDecoration(
+                  color: AppColors.inputBg,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(
+                      10,
+                    ),
+                  ),
+                ),
+                child: TextFormField(
+                  style: const TextStyle(color: AppColors.white),
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your email',
+                    hintStyle: TextStyle(
+                      color: AppColors.grayText,
+                    ),
+                    border: InputBorder.none,
+                    suffixIcon: Icon(Icons.visibility),
+                    suffixIconColor: AppColors.grayText,
+                  ),
+                  // validator: (String? value) {
+                  //   if (value == null || value.isEmpty) {
+                  //     return 'Please enter some text';
+                  //   }
+                  //   return null;
+                  // },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryBg = Color(0xFF161616);
+  static const Color primaryBlue = Color(0xFF6199FF);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0x0000000);
+  static const Color grayText = Color.fromRGBO(255, 255, 255, 0.50);
+  static const Color inputBg = Color.fromRGBO(255, 255, 255, 0.05);
+}
