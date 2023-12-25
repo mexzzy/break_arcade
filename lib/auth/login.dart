5295c5d095c80b6fd7cff0562eeb2842ee8e9774
mexzzy <samuelmeshach055@gmail.com>
2023-12-25 15:45:50 +0100

    login done

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -2,8 +2,15 @@ import 'package:break_arcade/auth/components/button.dart';
 import 'package:break_arcade/constants/app_colors.dart';
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _obscureText = true;
 
   @override
   Widget build(BuildContext context) {
@@ -11,114 +18,196 @@ class LoginScreen extends StatelessWidget {
       body: Container(
         color: AppColors.primaryBg,
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(
-                'assets/breakArcade_logo.png',
-                height: 50,
-                width: 50,
-                fit: BoxFit.cover,
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 16.0),
-                child: Text(
-                  'Break Arcade',
-                  style: TextStyle(
-                    fontSize: 35,
-                    fontWeight: FontWeight.bold,
-                    color: AppColors.white,
-                  ),
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  'assets/breakArcade_logo.png',
+                  height: 50,
+                  width: 50,
+                  fit: BoxFit.cover,
                 ),
-              ),
-              const Padding(
-                padding: EdgeInsets.only(top: 5.0),
-                child: Text(
-                  'Sign in to continue',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: AppColors.white,
-                  ),
-                ),
-              ),
-              const SizedBox(height: 40),
-              Container(
-                width: 0.8 * MediaQuery.of(context).size.width,
-                padding: const EdgeInsets.only(
-                  top: 5,
-                  right: 10,
-                  left: 10,
-                  bottom: 5,
-                ),
-                decoration: const BoxDecoration(
-                  color: AppColors.inputBg,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(
-                      10,
+                const Padding(
+                  padding: EdgeInsets.only(top: 16.0),
+                  child: Text(
+                    'Break Arcade',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.white,
                     ),
                   ),
                 ),
-                child: TextFormField(
-                  style: const TextStyle(color: AppColors.white),
-                  decoration: const InputDecoration(
-                    hintText: 'username',
-                    hintStyle: TextStyle(
+                const Padding(
+                  padding: EdgeInsets.only(top: 5.0),
+                  child: Text(
+                    'Sign in to continue',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: AppColors.white,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 40),
+                Container(
+                  width: 0.8 * MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.only(
+                    top: 5,
+                    right: 20,
+                    left: 20,
+                    bottom: 5,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: AppColors.inputBg,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: TextFormField(
+                    style: const TextStyle(color: AppColors.white),
+                    decoration: const InputDecoration(
+                      hintText: 'username',
+                      hintStyle: TextStyle(
+                        color: AppColors.grayText,
+                      ),
+                      border: InputBorder.none,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  width: 0.8 * MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.only(
+                    top: 5,
+                    right: 10,
+                    left: 20,
+                    bottom: 5,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: AppColors.inputBg,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: TextFormField(
+                    obscureText: _obscureText,
+                    style: const TextStyle(color: AppColors.white),
+                    decoration: InputDecoration(
+                      hintText: 'password',
+                      hintStyle: const TextStyle(
+                        color: AppColors.grayText,
+                      ),
+                      border: InputBorder.none,
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          _obscureText
+                              ? Icons.visibility
+                              : Icons.visibility_off,
+                          color: AppColors.grayText,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _obscureText = !_obscureText;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: 0.8 * MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.only(
+                    top: 10,
+                  ),
+                  child: const Text(
+                    'Forgot password?',
+                    textAlign: TextAlign.end,
+                    style: TextStyle(
                       color: AppColors.grayText,
                     ),
-                    border: InputBorder.none,
                   ),
-                  // validator: (String? value) {
-                  //   if (value == null || value.isEmpty) {
-                  //     return 'Please enter some text';
-                  //   }
-                  //   return null;
-                  // },
                 ),
-              ),
-              const SizedBox(height: 20),
-              Container(
-                width: 0.8 * MediaQuery.of(context).size.width,
-                padding: const EdgeInsets.only(
-                  top: 5,
-                  right: 10,
-                  left: 10,
-                  bottom: 5,
+                const SizedBox(height: 20),
+                CustomButton(
+                  text: 'Sign in',
+                  onPressed: () {
+                    print('Button Clicked!');
+                  },
                 ),
-                decoration: const BoxDecoration(
-                  color: AppColors.inputBg,
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(
-                      10,
+                const SizedBox(height: 40),
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    Container(
+                      width: 0.8 * MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: AppColors.inputBg,
+                          width: 1.0,
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-                child: TextFormField(
-                  style: const TextStyle(color: AppColors.white),
-                  decoration: const InputDecoration(
-                    hintText: 'password',
-                    hintStyle: TextStyle(
-                      color: AppColors.grayText,
+                    Container(
+                      color: AppColors.primaryBg,
+                      padding: const EdgeInsets.only(
+                        left: 10,
+                        right: 10,
+                      ),
+                      child: const Text(
+                        "Or continue with",
+                        style: TextStyle(
+                          color: AppColors.white,
+                        ),
+                      ),
                     ),
-                    border: InputBorder.none,
-                    suffixIcon: Icon(Icons.visibility),
-                    suffixIconColor: AppColors.grayText,
-                  ),
-                  // validator: (String? value) {
-                  //   if (value == null || value.isEmpty) {
-                  //     return 'Please enter some text';
-                  //   }
-                  //   return null;
-                  // },
+                  ],
                 ),
-              ),
-              const SizedBox(height: 20),
-              CustomButton(
-                text: 'Sign in',
-                onPressed: () {
-                  print('Button Clicked!');
-                },
-              ),
-            ],
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset(
+                      'assets/logos_google.png',
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.cover,
+                    ),
+                    const SizedBox(width: 20),
+                    Image.asset(
+                      'assets/logos_facebook.png',
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.cover,
+                    ),
+                    const SizedBox(width: 20),
+                    Image.asset(
+                      'assets/green_logo.png',
+                      height: 40,
+                      width: 40,
+                      fit: BoxFit.cover,
+                    )
+                  ],
+                ),
+                const SizedBox(height: 30),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Not a member ?",
+                      style: TextStyle(color: AppColors.grayText),
+                    ),
+                    Text(
+                      " Register now",
+                      style: TextStyle(
+                        color: AppColors.white,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
